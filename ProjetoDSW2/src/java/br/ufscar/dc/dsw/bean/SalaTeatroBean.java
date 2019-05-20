0b06ee5e78eb15f5cbd5a3d9a6f3f3f356d071f3
MarcosFelipe96 <cc.ufscar@gmail.com>
2019-05-20 11:00:44 -0300

    Adicionado SalaTeatroBean

---
+++ ProjetoDSW2/src/java/br/ufscar/dc/dsw/bean/SalaTeatroBean.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufscar.dc.dsw.bean;
+
+import br.ufscar.dc.dsw.dao.SalaTeatroDAO;
+import br.ufscar.dc.dsw.pojo.SalaTeatro;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Marcos Felipe
+ */
+@ManagedBean
+@SessionScoped 
+public class SalaTeatroBean implements Serializable {
+    
+    private SalaTeatro teatro;
+    
+    public String lista(){
+        return "teatro/index.xhtml";
+    }
+    
+    public String cadastra(){
+        teatro = new SalaTeatro();
+        return "form.xhtml?faces-redirect=true";
+    }
+    
+    public String edita(Long id){
+        SalaTeatroDAO dao = new SalaTeatroDAO();
+        teatro = dao.get(id);
+        return "form.xhtml";
+    }
+   
+    public String salva(){
+        SalaTeatroDAO dao = new SalaTeatroDAO();
+        if(teatro.getId() == null){
+            dao.save(teatro);
+        }else{
+            dao.update(teatro);
+        }
+        return "index.xhtml";
+    }
+    
+    public String delete(SalaTeatro teatro){
+        SalaTeatroDAO dao = new SalaTeatroDAO();
+        dao.delete(teatro);
+        return "index.xhtml";
+    }
+    
+    public String volta(){
+        return "index/.xhtml?faces-redirect=true";
+    }
+    
+    public List<SalaTeatro> getSalasTeatro() throws SQLException {
+        SalaTeatroDAO dao = new SalaTeatroDAO();
+        return dao.getAll();
+    }
+    
+    public SalaTeatro getSalaTeatro(){
+        return teatro;
+    }
+}
