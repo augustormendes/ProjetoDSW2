2eaa93fb0fa71344427b3c247557a267fe69d19d
DaanMoura <daniel.lucio.moura@gmail.com>
2019-05-20 10:41:34 -0300

    ADD: PromocaoDAO

--- ProjetoDSW2/src/java/br/ufscar/dc/dsw/dao/PromocaoDAO.java
+++ ProjetoDSW2/src/java/br/ufscar/dc/dsw/dao/PromocaoDAO.java
@@ -1,16 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufscar.dc.dsw.dao;
 
+import br.ufscar.dc.dsw.pojo.Promocao;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
-/**
- *
- * @author daniel
- */
-public class PromocaoDAO {
-    
+public class PromocaoDAO extends GenericDAO<Promocao> {
+    @Override
+    public Promocao get(Long id) {
+        EntityManager em = this.getEntityManager();
+        Promocao promocao = em.find(Promocao.class, id);
+        em.close();
+        return promocao;
+    }
+
+    @Override
+    public List<Promocao> getAll() {
+        EntityManager em = this.getEntityManager();
+        Query q = em.createQuery("select p from Promocao p", Promocao.class);
+        List<Promocao> promocoes = q.getResultList();
+        em.close();
+        return promocoes;
+    }
+
+    @Override
+    public void save(Promocao promocao) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(promocao);
+        tx.commit();
+        em.close();
+    }
+
+    @Override
+    public void update(Promocao promocao) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(promocao);
+        tx.commit();
+        em.close();
+    }
+
+    @Override
+    public void delete(Promocao promocao) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        promocao = em.getReference(Promocao.class, promocao.getId());
+        tx.begin();
+        em.remove(promocao);
+        tx.commit();
+    }
 }
