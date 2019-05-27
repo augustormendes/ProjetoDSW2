43981104e9e7296dfbf3808df3d1b1ea8c7be9de
MarcosFelipe96 <cc.ufscar@gmail.com>
2019-05-27 20:47:50 -0300

    Algumas correções e alteração na página inicial

--- ProjetoDSW2/src/java/br/ufscar/dc/dsw/AppConfig.java
+++ ProjetoDSW2/src/java/br/ufscar/dc/dsw/AppConfig.java
@@ -1,63 +1,63 @@
-package br.ufscar.dc.dsw;
+    package br.ufscar.dc.dsw;
 
-import javax.sql.DataSource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+    import javax.sql.DataSource;
+    import org.springframework.jdbc.datasource.DriverManagerDataSource;
+    import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableWebSecurity
-public class AppConfig extends WebSecurityConfigurerAdapter {
+    @EnableWebSecurity
+    public class AppConfig extends WebSecurityConfigurerAdapter {
 
-    private static DataSource dataSource;
+        private static DataSource dataSource;
 
-    public AppConfig() throws ClassNotFoundException {
-        dataSource = AppConfig.getDataSource();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder builder)
-            throws Exception {
-
-        String userSql = "SELECT email, senha, ativo FROM Usuario "
-                + " WHERE email = ?";
-        String roleSql = "select email, nome "
-                        + "from Papel where email = ?";
-        
-        builder.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery(userSql)
-                .authoritiesByUsernameQuery(roleSql)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/faces/site/form.xhtml").hasRole("ADMIN")
-                .antMatchers("/user/**").hasRole("USER")
-                .and()
-                .formLogin()
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/");
-
-        http.csrf().disable();
-    }
-
-    public static DataSource getDataSource() throws ClassNotFoundException {
-
-        if (dataSource == null) {
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-            String url = "jdbc:derby://localhost:1527/VendaIngressoBD";
-            String user = "root";
-            String password = "root";
-            dataSource = new DriverManagerDataSource(url, user, password);
+        public AppConfig() throws ClassNotFoundException {
+            dataSource = AppConfig.getDataSource();
         }
 
-        return dataSource;
+        @Override
+        public void configure(AuthenticationManagerBuilder builder)
+                throws Exception {
+
+            String userSql = "SELECT email, senha, ativo FROM Usuario "
+                    + " WHERE email = ?";
+            String roleSql = "select email, nome "
+                            + "from Papel where email = ?";
+
+            builder.jdbcAuthentication().dataSource(dataSource)
+                    .usersByUsernameQuery(userSql)
+                    .authoritiesByUsernameQuery(roleSql)
+                    .passwordEncoder(new BCryptPasswordEncoder());
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/faces/site/form.xhtml").hasRole("ADMIN")
+                    .antMatchers("/user/**").hasRole("USER")
+                    .and()
+                    .formLogin()
+                    .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/");
+
+            http.csrf().disable();
+        }
+
+        public static DataSource getDataSource() throws ClassNotFoundException {
+
+            if (dataSource == null) {
+                Class.forName("org.apache.derby.jdbc.ClientDriver");
+                String url = "jdbc:derby://localhost:1527/VendaIngressoBD";
+                String user = "root";
+                String password = "root";
+                dataSource = new DriverManagerDataSource(url, user, password);
+            }
+
+            return dataSource;
+        }
     }
-}
